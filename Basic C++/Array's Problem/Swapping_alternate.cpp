137e8856f134d8cb33fcb97928bdcc5daa186d0f
poonam kumawat <mailpoonam2002@gmail.com>
2022-09-26 00:16:52 +0530

    Array's Problem

---
+++ Basic C++/Array's Problem/Swapping_alternate.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<< " ";
+    }cout<<endl;
+}
+void swapAlternate(int arr[],int size){
+    for(int i=0;i<size;i+=2){
+        swap(arr[i],arr[i+1]);
+    }
+}
+int main(){
+    int even[8]={5,2,9,4,7,6,1,0};
+    int odd[5]={11,33,9,76,43};
+
+    swapAlternate(even ,8);
+    printArray(even, 8);
+    cout<<endl;
+    swapAlternate(odd ,5);
+    printArray(odd, 5);
+    return 0;
+
+}
