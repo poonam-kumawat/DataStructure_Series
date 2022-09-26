f50a64cc4e503ed7a568626b9afd9d75382fae88
poonam kumawat <mailpoonam2002@gmail.com>
2022-09-27 00:16:12 +0530

    Operators

---
+++ Basic C++/Operators/BitwiseOperator.cpp
@@ -0,0 +1,16 @@
+//Printing this Output
+// a&b 4
+// a|b 6
+// ~a -5
+// a^b 2
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4;
+    int b=6;
+    cout<< "a&b "<< (a&b)<<endl;
+    cout<< "a|b "<< (a|b)<<endl;
+    cout<< "~a "<< (~a )<<endl;
+    cout<< "a^b "<< (a^b) <<endl;
+
+}
