ae60c12398f3cc52dbc17d61e02ebd95cf0c032c
poonam kumawat <mailpoonam2002@gmail.com>
2022-10-03 22:57:27 +0530

    forloop

---
+++ Basic C++/Operators/forloop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the vale of n";
+    cin>>n;
+    cout<<"the value of n is"<<n<<endl;
+    for(int i=1;i<n;i++){
+        cout<<"the value of i is"<<i<<endl;
+    }
+}
