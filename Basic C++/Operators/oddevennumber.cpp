7b6d4b6335539fe33de29b07f73d11d3cdfab2f4
poonam kumawat <mailpoonam2002@gmail.com>
2022-10-07 21:46:50 +0530

    added odd number

---
+++ Basic C++/Operators/oddevennumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(n%2==0){
+        cout<<"Even Number"<<endl;
+
+    }
+    else{
+        cout<<"Odd Number"<<endl;
+    }
+
+    return 0;
+}
