9236c52a4ff3f72493f800bf5feb608f980c97ea
poonam kumawat <mailpoonam2002@gmail.com>
2022-12-30 23:49:50 +0530

    sort

--- Search/BinarySearch/sqrt.cpp
+++ Search/BinarySearch/sqrt.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 using namespace std;
 
+int binarySearch(int n)
+{
+    int s=0;e=n;
+    long long int mid=s+(e-s)/2;
+    long long int ans=-1;
+    while(s<=e){
+        if(mid*mid==n){
+        return mid;
+    }
+        
+    if(mid*mid<n){
+        
+        ans=mid;
+        s=mid+1;
 
+    }else{
+        e=mid-1;
+    }
+    mid=s+(e-s)/2;
+    
+    }
+    retrun ans;
+}
+int getSqrt(int x){
+    return binarySearch(x);
+}
 int main(){
 
 }
