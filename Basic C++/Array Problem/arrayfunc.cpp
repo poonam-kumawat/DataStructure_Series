63644e23de9ff06a0f3ab64934740ad9253f845a
poonam-kumawat <mailpoonam2002@gmail.com>
2023-01-02 23:51:57 +0530

    insertion

--- Basic C++/Array Problem/arrayfunc.cpp
+++ Basic C++/Array Problem/arrayfunc.cpp
@@ -2,25 +2,17 @@
 using namespace std;
 void printArray(int arr[],int size){
     cout<<"Printing the array"<<endl;
-    //printing
+    //Printing
     for(int i=0;i<size;i++){
         cout<<arr[i]<<" ";
     }
-
 }
-
 int main(){
     int third[15];
     int forth[5];
     printArray(third ,15);
-
     printArray(forth ,5);
     int forthsize=sizeof(forth)/sizeof(int);
     cout<<"Size of Forth is  "<<forthsize;
-
-    return 0;
-
-    
-
-    
+    return 0;    
 }
