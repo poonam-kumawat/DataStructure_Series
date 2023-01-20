f36a6e0af951834179b41349ef90bd9ff01068c5
poonam-kumawat <mailpoonam2002@gmail.com>
2023-01-20 22:57:26 +0530

    charArray

--- 6.String Array/chararray.cpp
+++ 6.String Array/chararray.cpp
@@ -1,9 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+int charLength(char name[]){
+    int c=0;
+    for(int i=0;name[i]!='\0';i++){
+        c++;
+    }
+    return c;
+}
+
 int main(){
     char name[20];
     cout<<"Enter name"<<endl;;
     cin>>name;
-    cout<<name;
+    cout<<name<<endl;
+
+    cout<<"Length "<<charLength(name)<<endl;
+    return 0;
     
 } 
+
+
+
