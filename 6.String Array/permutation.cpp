f25609b86a97e1566ff2f73ddaeba404f722ffaf
poonam-kumawat <mailpoonam2002@gmail.com>
2023-01-23 23:04:48 +0530

    permutation string

---
+++ 6.String Array/permutation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+bool checkInclusion(string s1,string s2){
+    int count[26]={0};
+    for(int i=0;i<s1.length();i++){
+        int index=s1[i]-'a';
+        count[index]++;
+    }
+}
+
+int main(){
+
+}
