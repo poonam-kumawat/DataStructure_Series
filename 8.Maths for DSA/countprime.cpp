688317ee38d8fb98d86003102bf3cb20a14729a7
poonam-kumawat <mailpoonam2002@gmail.com>
2023-01-27 10:51:19 +0530

    count prime

--- 8.Maths for DSA/countprime.cpp
+++ 8.Maths for DSA/countprime.cpp
@@ -1,22 +1,15 @@
-bool isPrime(int n){
-    if(n<=1){
-        return false;
-    }
-    for(int i=2;i,n;i++){
-        if(n%i==0){
-            return false;
-        }
-        
-    }
-    return true;
-}
 
 int countPrime(int n){
     int cnt=0;
+    vector<bool> prime(n+1,true);
+    prime[0]=prime[1]=false;
     for(int i=2;i<n;i++){
-        if(isPrime(i)){
+        if(prime[i]){
             cnt++;
+            for(int j=2*i;j<n;j+i){
+                prime[j]=0;
+            }
         }
-        return cnt;
     }
+    return cnt;
 }
