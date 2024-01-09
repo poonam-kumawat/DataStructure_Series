ecc323fc96e4bd1a583bd2aa287819b35cc184d1
Poonam kumawat <103033530+poonam-kumawat@users.noreply.github.com>
2024-01-09 12:21:04 +0530

    Create zig-zagPattern.cpp

---
+++ Patterns/zig-zagPattern.cpp
@@ -0,0 +1,30 @@
+//     *       *     
+//   *   *   *   *   
+// *       *       * 
+
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // Write C++ code here
+   int n=9;
+   for(int i=1;i<=3;i++){
+       for(int j=1;j<=n;j++){
+           if(((i+j)%4==0) || (i==2 && j%4==0)){
+               cout<<"* ";
+           }
+           else{
+               cout<<"  ";
+           }
+       }
+       
+   
+      cout<<endl;
+   }
+
+
+    return 0;
+}
