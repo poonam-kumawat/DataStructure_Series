03a07db77dc80c73d1ccf61885e5891f98d81298
Poonam kumawat <103033530+poonam-kumawat@users.noreply.github.com>
2024-01-09 12:04:07 +0530

    Create starTriangle.cpp

---
+++ Patterns/starTriangle.cpp
@@ -0,0 +1,47 @@
+//      * 
+//    * * * 
+//   * * * * * 
+//  * * * * * * * 
+// * * * * * * * * * 
+// * * * * * * * * * 
+//  * * * * * * * 
+//   * * * * * 
+//    * * * 
+//     * 
+
+
+
+
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // Write C++ code here
+   int n=5;
+   for(int i=1;i<=n;i++){
+    //   int j;
+       for(int j=1;j<=n-i;j++){
+         cout<<" ";
+       }
+      for(int j=1;j<=2*i-1;j++){
+          cout<<"* ";
+      }
+      cout<<endl;
+   }
+    for(int i=n;i>=1;i--){
+    //   int j;
+       for(int j=1;j<=n-i;j++){
+         cout<<" ";
+       }
+      for(int j=1;j<=2*i-1;j++){
+          cout<<"* ";
+      }
+      cout<<endl;
+   }
+
+
+    return 0;
+}
